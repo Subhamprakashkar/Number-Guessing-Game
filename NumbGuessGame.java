f5061afec41af355d6a603ac16223cc0842b7bfb
Subhamprakashkar <karsubhampraksh@gmail.com>
2024-08-24 17:07:54 +0530

    Add files via upload

---
+++ NumbGuessGame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumbGuessGame {
+  public static void main(String [] args){
+    Scanner reader = new Scanner(System.in);
+    String play = "yes";
+    // while loop to determine if we are going to play the game again
+    while (play.equals("yes")) {
+        Random rand = new Random();
+        int randNum = rand.nextInt(100);
+        int guess = -1;
+        int tries = 0;
+
+     //while loop to check if the game is over 
+     while(guess != randNum){
+        System.out.println("Guess a number between 1 and 100");
+        guess = reader.nextInt();
+        tries++;
+
+        if(guess == randNum){
+            System.out.println("Awesome you guessed the number: ");
+            System.out.println("It only took " + tries + "guesses");
+            System.out.println("would you like to play again yes or no: ");
+            play = reader.next().toLowerCase();
+        }
+        else if(guess > randNum){
+            System.out.println("your guess is too high , try again ");
+        }
+        else{
+            System.out.println("your guess is too low , tey again");
+        }
+     }
+    }
+    reader.close();
+  }    
+}
